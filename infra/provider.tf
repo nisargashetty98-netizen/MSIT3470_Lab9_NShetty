bcabde2aa13adc089b9b984cf01037fb97cffd1c
Nisarga <240807695+nisargashetty98-netizen@users.noreply.github.com>
2025-11-04 23:17:58 -0500

    Fix: formatted provider.tf properly

--- infra/provider.tf
+++ infra/provider.tf
@@ -13,7 +13,7 @@ terraform {
 }
 
 provider "aws" {
-  region                      = "us-east-1"
+  region = "us-east-1"
 
   # ✅ These lines stop Terraform from checking AWS credentials
   skip_credentials_validation = true
