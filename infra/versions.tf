9e5c24176b40ba46251e8f7c821bd2b23266c82f
Nisarga <240807695+nisargashetty98-netizen@users.noreply.github.com>
2025-11-04 23:04:16 -0500

    Fix: update Terraform version constraint to allow 1.13.x

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,14 +1,8 @@
 terraform {
-  required_version = "1.8.5"
+  required_version = ">= 1.8.0, < 2.0.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.64"
-    }
-    random = {
+      random = {
       source  = "hashicorp/random"
       version = "~> 3.6"
     }
-  }
 }
