b7b04e7d33bd41f5d5a5fbfd2a7bc17141c9b6af
Nisarga <240807695+nisargashetty98-netizen@users.noreply.github.com>
2025-11-04 22:42:25 -0500

    Fix: formatted Terraform files

--- infra/provider.tf
+++ infra/provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region                      = "us-east-1"
+  region = "us-east-1"
 
   # These flags let CI run without real AWS credentials
   skip_credentials_validation = true
